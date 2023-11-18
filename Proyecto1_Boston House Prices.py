4f7a46c9f1e1fd3ed10107a86da033464ab6cb03
yancondori <yan.condori@gmail.com>
2023-11-18 16:37:14 +0100

    inicilizando tarea

---
+++ Proyecto1_Boston House Prices.py
@@ -0,0 +1,51 @@
+
+# Proyecto 1.b :  "Boston Housing Prices"
+# Boston House Prices-Advanced Regression Techniques
+
+# Tarea 1: Comprensión del Conjunto de Datos
+# Descargar el conjunto de datos "Boston Housing Prices" desde Kaggle.
+# Rpta: La datos se descargaron de  https://www.kaggle.com/datasets/fedesoriano/the-boston-houseprice-data/
+
+# Explorar la documentación y la descripción del conjunto de datos para comprender sus características y variables.
+import pandas as pd
+import numpy as np
+
+
+
+
+
+boston_hp = pd.read_csv('boston.csv')
+
+
+# Cargar el conjunto de datos en un entorno de análisis (Colab) y examinar las primeras filas para una comprensión inicial.
+ 
+
+# Tarea 2: Análisis Exploratorio de Datos (EDA)
+
+# Realizar un análisis descriptivo de las variables, incluyendo estadísticas resumidas y visualización de distribuciones individuales.
+# Identificar valores atípicos (outliers) y decidir cómo manejarlos (eliminarlos, imputarlos por la media, etc).
+# Visualizar relaciones entre variables mediante gráficos de dispersión y matriz de correlación.
+ 
+
+# Tarea 3: Preprocesamiento de Datos
+
+# Manejar valores faltantes si es necesario (imputación o eliminación).
+# Escalar las variables numéricas si es necesario.
+# Dividir el conjunto de datos en un conjunto de entrenamiento y un conjunto de prueba.
+ 
+
+# Tarea 4 Comunicación de Resultados
+
+# Crear un informe técnico que incluya una descripción detallada de los gráficos y visualizaciones.
+# Preparar una presentación para comunicar los resultados a un público no técnico.
+ 
+
+# Tarea 5: Documentación y Código
+
+# Documentar el código de manera clara y detallada.
+# Utilizar control de versiones para rastrear cambios en el código. (Opcional)
+ 
+
+# Tarea 6: Entrega Final
+
+# Entregar el informe técnico, la presentación y el código del proyecto.
